b415fd3f4312dc73f701e8ac685df193a4e66dc0
omerincirkus <72596679+omerincirkus@users.noreply.github.com>
2021-09-25 16:07:08 +0300

    added card and completed homepage and cryptocurrencies

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,14 +3,15 @@ import millify from 'millify'
 import { Typography, Row, Col, Statistic } from 'antd'
 import { Link } from 'react-router-dom'
 import { useGetCryptosQuery } from '../services/cryptoApi'
+import { Cryptocurrencies, News } from '../components/index'
 
 const Homepage = () => {
   const { Title } = Typography
-  const { data, isFetching } = useGetCryptosQuery()
+  const { data, isFetching } = useGetCryptosQuery(10)
   const globalStats = data?.data?.stats
   console.log(data)
 
-  if (isFetching) return 'Loding...'
+  if (isFetching) return 'Loading...'
 
   return (
     <>
@@ -46,6 +47,24 @@ const Homepage = () => {
           />
         </Col>
       </Row>
+      <div className='home-heading-container'>
+        <Title level={2} className='home-title'>
+          Top 10 Cryptocurrencies in the world
+        </Title>
+        <Title level={3} className='show-more'>
+          <Link to='/cryptocurrencies'>Show More</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className='home-heading-container'>
+        <Title level={2} className='home-title'>
+          Latest Crypto News
+        </Title>
+        <Title level={3} className='show-more'>
+          <Link to='/news'>News</Link>
+        </Title>
+      </div>
+      <News simplified />
     </>
   )
 }
